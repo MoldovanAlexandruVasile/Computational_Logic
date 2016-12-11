1ae0b9041c38ea721df1f7bc42045a530918df2b
MoldovanAlexandruVasile <moldovan_alexandru97@yahoo.com>
2016-12-11 12:24:20 +0200

    Operations and conversions

---
+++ ui.py
@@ -0,0 +1,19 @@
+'''
+
+@author: Moldovan Alexandru-Vasile
+Group: 915
+
+'''
+
+
+def menu():
+
+    print("\n \n     @author: Moldovan Alexandru-Vasile")
+    print("\n \n  =========================================")
+    print("\n \t    ~~Menu~~")
+    print("\n \t 1. Addition")
+    print("\n \t 2. Subtraction")
+    print("\n \t 3. Multiplication with one digit")
+    print("\n \t 4. Division with one digit")
+    print("\n \t 5. Conversion")
+    print("\n \t 0. Exit \n")
